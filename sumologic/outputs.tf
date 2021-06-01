3c5d148131a8274160dbd8921e34a4faa2bfc949
sourabh <sourabh@sumologic.com>
2021-06-01 23:08:55 +0530

    adding sumologic module

---
+++ sumologic/outputs.tf
@@ -0,0 +1,19 @@
+output "sumologic_field" {
+  value = sumologic_field.SumoLogicFields
+  description = "This output contains all the fields."
+}
+
+output "sumologic_field_extraction_rule" {
+  value = sumologic_field_extraction_rule.SumoLogicFieldExtractionRules
+  description = "This output contains all the Field Extraction rules."
+}
+
+output "sumologic_content" {
+  value = sumologic_content.SumoLogicApps
+  description = "This output contains all the Apps."
+}
+
+output "sumologic_metric_rules" {
+  value = null_resource.MetricRules
+  description = "This output contains all the metric rules."
+}
