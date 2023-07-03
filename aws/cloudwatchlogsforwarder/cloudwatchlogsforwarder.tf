2a929f73499c60cda4f536af36ebdf7f2fbd4d85
Himanshu Sharma <hsharma@sumologic.com>
2023-07-03 15:28:52 +0530

    Parameterising sam version variable

--- aws/cloudwatchlogsforwarder/cloudwatchlogsforwarder.tf
+++ aws/cloudwatchlogsforwarder/cloudwatchlogsforwarder.tf
@@ -190,7 +190,7 @@ resource "aws_serverlessapplicationrepository_cloudformation_stack" "auto_enable
 
   name             = "Auto-Enable-Logs-Subscription-${random_string.aws_random.id}"
   application_id   = "arn:aws:serverlessrepo:us-east-1:956882708938:applications/sumologic-loggroup-connector"
-  semantic_version = "1.0.6"
+  semantic_version = var.app_semantic_version
   capabilities     = data.aws_serverlessapplicationrepository_application.app.required_capabilities
   parameters = {
     DestinationArnType  = "Lambda"
