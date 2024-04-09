ab238f479dcadf7bb4e1ed0a4a117a91602c183b
Himanshu Sharma <hsharma@sumologic.com>
2024-04-09 21:14:31 +0530

    Updating kinesisfirehose for logs example

--- aws/kinesisfirehoseforlogs/examples/default/main.tf
+++ aws/kinesisfirehoseforlogs/examples/default/main.tf
@@ -1,19 +1,25 @@
+resource "random_string" "aws_random" {
+  length  = 10
+  upper   = false
+  special = false
+}
+
 module "kinesis_firehose_for_logs_module" {
   source = "SumoLogic/sumo-logic-integrations/sumologic//aws/kinesisfirehoseforlogs"
 
   create_collector = true
 
   source_details = {
-    source_name     = "<Source-Name>"
-    source_category = "<Source-Category>"
-    description     = "<Source-Description>"
+    source_name     = "Cloud Watch Logs (Region)"
+    source_category = "aws/observability/cloudwatch/logs"
+    description     = "This source is created using Sumo Logic terraform AWS Observability module to collect AWS Cloud Watch logs."
     collector_id    = null
     fields          = {}
   }
 
   create_bucket = true
   bucket_details = {
-    bucket_name          = "<AWS-S3-bucket>"
+    bucket_name          = local.bucket_name
     force_destroy_bucket = false
   }
 
