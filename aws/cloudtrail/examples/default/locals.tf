12e374dc165bba2dc20ae3c31ba3deed31e0f4ea
Akhil Dangore <akhil.dangore.ctr@sumologic.com>
2024-04-10 11:28:46 +0530

    Remove unused var

--- aws/cloudtrail/examples/default/locals.tf
+++ aws/cloudtrail/examples/default/locals.tf
@@ -7,5 +7,4 @@ locals {
   bucket_name          = "aws-observability-random-${random_string.aws_random.id}"
   path_expression      = "AWSLogs/${local.aws_account_id}/CloudTrail/${local.aws_region}/*"
   cloudtrail_fields      = { account = local.aws_account_id }
-  cloudtrail_source_bucket_name = "akhil_${local.aws_region}_${var.sumologic_organization_id}"
 }
