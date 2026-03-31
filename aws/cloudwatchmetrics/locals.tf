2d236f3e8ab67df4c276b0a5772830e5d7353830
Akhil Dangore <akhil.dangore.ctr@sumologic.com>
2026-03-31 12:15:13 +0530

    Updated SUMO_LOGIC_AWS_ACCOUNT_ID in cloudwatchmetrics

--- aws/cloudwatchmetrics/locals.tf
+++ aws/cloudwatchmetrics/locals.tf
@@ -1,5 +1,13 @@
 locals {
 
+  # sumo aws account ids
+  sumo_account_ids = {
+    aws        = "926226587429"  # Commercial AWS account
+    aws-us-gov = "926226587429"   # GovCloud account
+    aws-cn     = "926226587429"   # China account
+    aws-eusc   = "052162193518"   # EU Sovereign account
+  }
+
   # Get the default collector name if no collector name is provided.
   collector_name = var.collector_details.collector_name == "SumoLogic CloudWatch Metrics Collector <Random ID>" ? "SumoLogic CloudWatch Metrics Collector ${random_string.aws_random.id}" : var.collector_details.collector_name
 
