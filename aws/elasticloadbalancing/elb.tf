ca74ba81840def15ca27c92eaa3267c779e96804
Md Azhar <md.azhar@sumologic.com>
2026-08-21 11:25:10 +0530

    Removed local path comment

--- aws/elasticloadbalancing/elb.tf
+++ aws/elasticloadbalancing/elb.tf
@@ -200,7 +200,6 @@ resource "aws_sns_topic_subscription" "subscription" {
 
 module "auto_enable_access_logs_module" {
   depends_on = [sumologic_elb_source.source]
-  #source = "/Users/akhil.dangore.ctr/Documents/ProjectSource/terraform-sumologic-sumo-logic-integrations/aws/autoenable/modules/s3_logging"
   source = "git::https://github.com/SumoLogic/terraform-sumologic-sumo-logic-integrations.git//aws/autoenable/modules/s3_logging?ref=fy27q2"
 
   providers = {
